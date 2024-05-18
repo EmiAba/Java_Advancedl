cc0258165b31261bcd8103ab7c712c83dc86067d
EmiAba <abadjieva@gmail.com>
2024-05-18 13:49:08 +0300

    Update _04_Maximal_Sum.java

--- 04_Multidimensional_Arrays_Exc/_04_Maximal_Sum.java
+++ 04_Multidimensional_Arrays_Exc/_04_Maximal_Sum.java
@@ -4,26 +4,54 @@ import java.util.Scanner;
 
 public class _04_Maximal_Sum {
     public static void main(String[] args) {
-Scanner scanner  =new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
+        int row = scanner.nextInt();
+        int col = scanner.nextInt();
+        int maxSum = Integer.MIN_VALUE;
+        int[][] matrix = new int[row][col];
+        int maxMatrixRow = 0;
+        int maxMatrixCol = 0;
 
-    int row= scanner.nextInt();
-    int col= scanner.nextInt();
+        fillMatrix(scanner, matrix);
+        int sumMatrix;
 
-    int[][] matrix=new int[row][col];
+        // Loop through the matrix, but avoid the last two rows and columns to stay within bounds
+        for (int r = 0; r < matrix.length - 2; r++) {
+            for (int c = 0; c < matrix[r].length - 2; c++) {
+                sumMatrix = matrix[r][c] + matrix[r][c+1] + matrix[r][c+2] +
+                        matrix[r+1][c] + matrix[r+1][c+1] + matrix[r+1][c+2] +
+                        matrix[r+2][c] + matrix[r+2][c+1] + matrix[r+2][c+2];
 
-        fillMatrix(scanner,  matrix);
+                // Debugging output to track the sum of each 3x3 submatrix
+               // System.out.printf("Sum of submatrix starting at (%d, %d): %d%n", r, c, sumMatrix);
 
+                if (sumMatrix > maxSum) {
+                    maxSum = sumMatrix;
+                    maxMatrixRow = r;
+                    maxMatrixCol = c;
+                }
+            }
+        }
 
-
-
+        System.out.printf("Sum=%d%n", maxSum);
+        printMatrix(matrix, maxMatrixRow, maxMatrixCol);
     }
-    private static void fillMatrix(Scanner scanner,  int[][] matrix) {
+
+    private static void printMatrix(int[][] matrix, int startRow, int startCol) {
+        for (int row = startRow; row < startRow + 3; row++) {
+            for (int col = startCol; col < startCol + 3; col++) {
+                System.out.print(matrix[row][col] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    private static void fillMatrix(Scanner scanner, int[][] matrix) {
         for (int row = 0; row < matrix.length; row++) {
             for (int col = 0; col < matrix[row].length; col++) {
                 matrix[row][col] = scanner.nextInt();
             }
-
         }
     }
 }
